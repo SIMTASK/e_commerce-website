200ba37a2db78c42ccad5e91e8db4c55d94d2ee2
SIMTASK <138787642+SIMTASK@users.noreply.github.com>
2023-07-11 11:22:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function addToCart(productId) {
+    // Perform logic to add the selected product to the cart
+    // You can implement your own logic here
+    // For simplicity, let's just log the product ID
+    console.log("Product ID: " + productId + " added to cart.");
+}
